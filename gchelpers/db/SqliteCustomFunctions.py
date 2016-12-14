63dfe0a4d0844a36402a618b5cc04994bcfc4416
unknown <mseyer@JOHNNY-B.g-cpartners.local>
2016-12-14 17:41:12 -0600

    Added additional sqlite functions

--- gchelpers/db/SqliteCustomFunctions.py
+++ gchelpers/db/SqliteCustomFunctions.py
@@ -8,8 +8,25 @@ from gchelpers.ip.GeoDbManager import GeoDbManager
 
 GEO_MANAGER = GeoDbManager()
 
+def splitpath(fullpath):
+    allparts = []
+    while 1:
+        parts = os.path.split(fullpath)
+        if parts[0] == fullpath:  # sentinel for absolute paths
+            allparts.insert(0, parts[0])
+            break
+        elif parts[1] == fullpath: # sentinel for relative paths
+            allparts.insert(0, parts[1])
+            break
+        else:
+            fullpath = parts[0]
+            allparts.insert(0, parts[1])
+    return allparts
+
 def RegisterSQLiteFunctions(dbh):
     dbh.create_function("REGEXP", 2, Regexp)
+    dbh.create_function('Basename',1,Basename)
+    dbh.create_function('BasenameN',1,BasenameN)
     dbh.create_function("GetRegMatch", 3, GetRegMatch)
     dbh.create_function("GetRegMatchArray", 3, GetRegMatchArray)
     dbh.create_function("RemoveNewLines", 1, RemoveNewLines)
@@ -29,6 +46,37 @@ def Regexp(pattern,input):
     except Exception as error:
         print(u'ERROR: {}'.format(str(error)))
         return False
+
+def Basename(fullname):
+    '''Get the base name of a fullname string'''
+    value = ''
+    if fullname:
+        try:
+            value = os.path.basename(fullname)
+        except:
+            value = filename
+        
+    return value
+
+def BasenameN(fullname,n):
+    '''Get the base name of a fullname string'''
+    value = ''
+    if fullname:
+        spitname = splitpath(fullname)
+        if spitname[0] == '':
+            spitname.pop(0)
+            
+        if len(spitname) < n:
+            value = os.path.join(
+                *spitname
+            )
+        else:
+            value = os.path.join(
+                *spitname[-(n):]
+            )
+            
+    return value
+    
 
 def GetIpInfo(ip_address):
     if ip_address is None:
