5e4537fc99d4c31f5d2191628c31bd5323f6e661
unknown <mseyer@JOHNNY-B.g-cpartners.local>
2017-02-14 10:02:41 -0600

    Added python-dateutil to install_requires

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ setup(
     zip_safe=False,
     install_requires=[
         'geoip2',
-        'pyyaml'
+        'pyyaml',
+        'python-dateutil'
     ],
     packages=find_packages(
         '.',
